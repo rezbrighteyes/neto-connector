f615a985f68e3483b8b6c63db70858db825fbc88
Reza <reza@brighteyes.com.au>
2026-04-28 06:49:16 +1000

    feat: add neto_date_paid and neto_payment_method fields to sale.order

--- models/sale_order.py
+++ models/sale_order.py
@@ -7,3 +7,5 @@ class SaleOrder(models.Model):
 
     neto_order_id = fields.Char(string='Neto Order ID', index=True, copy=False)
     neto_order_status = fields.Char(string='Neto Status', copy=False, readonly=True)
+    neto_date_paid = fields.Datetime(string='Neto Date Paid', copy=False, readonly=True)
+    neto_payment_method = fields.Char(string='Neto Payment Method', copy=False, readonly=True)
