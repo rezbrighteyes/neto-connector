c11b28b362052a689e1acad98ab098c22d971763
Amirreza Delavaran Shiraz <rez@Amirrezas-MacBook-Pro.local>
2026-05-22 12:37:06 +1000

    feat: add customer Neto payment smart button

--- models/res_partner.py
+++ models/res_partner.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from odoo import models, fields
+from odoo import api, fields, models
 
 
 class ResPartner(models.Model):
@@ -11,3 +11,33 @@ class ResPartner(models.Model):
     neto_on_credit_hold = fields.Boolean(string='On Credit Hold (Neto)', default=False)
     neto_classification = fields.Char(string='Neto Classification')
     neto_last_sync = fields.Datetime(string='Neto Last Sync', readonly=True)
+    neto_payment_count = fields.Integer(
+        string='Neto Payments',
+        compute='_compute_neto_payment_count',
+    )
+
+    @api.depends('child_ids')
+    def _compute_neto_payment_count(self):
+        Payment = self.env['neto.payment'].sudo()
+        for partner in self:
+            commercial_partner = partner.commercial_partner_id or partner
+            partner_ids = self.search([
+                ('id', 'child_of', commercial_partner.id),
+            ]).ids
+            partner.neto_payment_count = Payment.search_count([
+                ('partner_id', 'in', partner_ids),
+            ])
+
+    def action_view_neto_payments(self):
+        self.ensure_one()
+        commercial_partner = self.commercial_partner_id or self
+        partner_ids = self.search([
+            ('id', 'child_of', commercial_partner.id),
+        ]).ids
+        action = self.env['ir.actions.act_window']._for_xml_id(
+            'Reza_neto_connector.neto_payment_action'
+        )
+        action['domain'] = [('partner_id', 'in', partner_ids)]
+        action['context'] = {'default_partner_id': self.id}
+        action['name'] = 'Neto Payments'
+        return action
